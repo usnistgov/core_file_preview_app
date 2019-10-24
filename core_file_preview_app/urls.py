2b36cd8e0b61190d72340a5325539f88db5cb10f
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:17 -0400

    fix: revert django upgrade
    Change-Id: Ie186de0028b6cb7b537964d52e0d02fab613e435

--- core_file_preview_app/urls.py
+++ core_file_preview_app/urls.py
@@ -1,11 +1,10 @@
 """ Url router for the file preview app
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_file_preview_app.views.user.ajax import get_blob_preview
 
 urlpatterns = [
-    re_path(r'^get_blob_preview', get_blob_preview,
-            name='core_file_preview_app_get_blob_preview'),
+    url(r'^get_blob_preview', get_blob_preview,
+        name='core_file_preview_app_get_blob_preview'),
 ]
