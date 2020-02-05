f5134979d9efb73bb49efcae923a7b627d4a006c
Augustin CHINI <augustin.chini@nist.gov>
2020-02-05 10:06:56 -0500

    chore: upgrade 1.6.0
    Change-Id: Ifb9e4a4794f78ba73a9ca376c9cdd535c64c5ffc

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_file_preview_app",
-    version="1.5.0",
+    version="1.6.0",
     description="File preview functionalities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
