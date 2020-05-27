103eeb061ba69724eacdc549193c855057a798b5
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-05-27 09:55:42 -0400

    chore: apply black formatting
    Change-Id: I0b1f861d49a44083a3c3d34425aeecf283e94123

--- core_file_preview_app/urls.py
+++ core_file_preview_app/urls.py
@@ -6,6 +6,9 @@ from django.urls import re_path
 from core_file_preview_app.views.user.ajax import get_blob_preview
 
 urlpatterns = [
-    re_path(r'^get_blob_preview', get_blob_preview,
-            name='core_file_preview_app_get_blob_preview'),
+    re_path(
+        r"^get_blob_preview",
+        get_blob_preview,
+        name="core_file_preview_app_get_blob_preview",
+    ),
 ]
--- core_file_preview_app/views/user/ajax.py
+++ core_file_preview_app/views/user/ajax.py
@@ -7,7 +7,10 @@ from django.http.response import HttpResponse, HttpResponseServerError
 from rest_framework import status
 
 from core_main_app.utils.blob_downloader import BlobDownloader
-from core_main_app.utils.file import get_filename_from_response, get_base_64_content_from_response
+from core_main_app.utils.file import (
+    get_filename_from_response,
+    get_base_64_content_from_response,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -37,27 +40,29 @@ def get_blob_preview(request):
           content: Internal server error
     """
     try:
-        url = request.GET.get('url_blob', None)
+        url = request.GET.get("url_blob", None)
         if url is not None:
             # download the blob
-            response = BlobDownloader(url, request.session.session_key).get_blob_response()
+            response = BlobDownloader(
+                url, request.session.session_key
+            ).get_blob_response()
             # manage the response
             if response is not None:
                 if response.status_code == status.HTTP_200_OK:
                     # build content
                     content = {
-                         "content": get_base_64_content_from_response(response),
-                         "mime_type": response.headers["Content-type"],
-                         "filename": get_filename_from_response(response)
+                        "content": get_base_64_content_from_response(response),
+                        "mime_type": response.headers["Content-type"],
+                        "filename": get_filename_from_response(response),
                     }
                     # we re-build the response from the response received
-                    return HttpResponse(json.dumps(content),
-                                        'application/json')
+                    return HttpResponse(json.dumps(content), "application/json")
                 else:
-                    logger.error("get_blob_preview: Error while getting the blob: {0} status code: {1}".format(
-                        json.loads(response.text)["message"],
-                        response.status_code
-                    ))
+                    logger.error(
+                        "get_blob_preview: Error while getting the blob: {0} status code: {1}".format(
+                            json.loads(response.text)["message"], response.status_code
+                        )
+                    )
                     content_message = "Something went wrong while getting the file."
             else:
                 # at this point there is no known endpoint in our system
