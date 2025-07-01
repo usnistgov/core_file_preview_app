08753d0501c504d8b122a733c0715344f148acb2
Guillaume Sousa <guillaume.sousa@nist.gov>
2025-07-01 19:09:49 +0000

    chore: upgrade 2.17.0
    Change-Id: I50e7d7668d0996362a812f2a56c5e4094ac3e80d

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_file_preview_app",
-    version="2.16.0",
+    version="2.17.0",
     description="File preview functionalities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
